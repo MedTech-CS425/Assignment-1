ee8a1f7394247514b71bb989317bf82b2cd3c2ab
khalilazaiez <azaiez.khalil@medtech.tn>
2021-01-11 20:49:43 +0100

    sign in

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const User = require('../models/user.model');
 const router = express.Router();
+const jwtHelper = require('../config/jwtHelper');
 const ctrlUser = require('../controller/user.controller');
 router.post('/register', ctrlUser.register);
 router.post('/authenticate', ctrlUser.authenticate);
+router.get('/userProfile',jwtHelper.verifyJwtToken, ctrlUser.userProfile);
 module.exports = router;
