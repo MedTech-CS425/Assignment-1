33a568b93c27c7e04ce76c6cf05b641dd823c2d7
selimzr <zerelliselim@gmail.com>
2021-05-07 21:40:59 +0100

    Assignment

---
+++ models/item.model.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose');
+
+const itemSchema=mongoose.Schema({
+
+    name:String,
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users'
+
+    },
+    category_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'categorys'
+    },
+    note:String
+        
+
+    ,
+    image:String,
+
+    createdAt:{
+        type:Date,
+        default:Date.now()
+
+    },
+    updatedAt:{
+        type:Date,
+        default:Date.now()
+    }
+});
+
+module.exports=mongoose.model('item',itemSchema)
